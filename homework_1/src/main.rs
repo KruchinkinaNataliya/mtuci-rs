6c6d34da5ff75891acaf219437e4deeed4242b94
natakru <nata.kruchinkina.1999@mail.ru>
2023-06-30 03:26:41 +0300

    homework_1

--- homework_1/src/main.rs
+++ homework_1/src/main.rs
@@ -1,3 +1,30 @@
+// fn main() {
+//     for i in 1..=100{
+//         if i%15 == 0 {
+//             println!("FizzBuzz"); }
+//         else {
+//             if i%3 == 0 {
+//                 println!("Fizz"); }
+//             else {
+//                 if i%5 == 0 {
+//                     println!("Buzz"); }
+//                 else {
+//                     println!("{i}");}
+//             };
+//         };
+//     };
+//  }
+
 fn main() {
-    println!("Hello, world!");
+    for i in 1..=100 {
+        match (i%5 == 0, i%3 ==0) {
+            (false, true) =>
+                println!("Fizz"),
+            (true, false) =>
+                println!("Buzz"),
+            (true, true) =>
+                println!("FizzBuzz"),
+            _ =>
+                println!("{i}") };
+    };
 }
